b6037e0e5da467f5019273ee7e59877191df9538
Julian Biebelberg <jbiebelberg@gmail.com>
2016-06-09 18:30:36 -0400

    updated javascript

--- ajax.js
+++ ajax.js
@@ -1,15 +1,25 @@
 $( document ).ready(function() {
 
-var list = ['batman', 'superman', 'captainamerica', 'spiderman', 'ironman', 'hulk', 'thor', 'windersoldier', 'greenlantern', 'deadpool']
+var list = ['batman', 'superman', 'captainamerica', 'spiderman', 'ironman', 'hulk', 'thor', 'windersoldier', 'greenlantern', 'deadpool'];
 
 for(i=0; i<list.length; i++){
 	
 	var newButton = $('<button>' + list[i] + '</button>');
 
-	
-
 	$('#list').append(newButton);
-}
+};
+
+
+$('#submit').on('click', function(){
+
+	var searchTerm = $('#text').val();
+
+	$.ajax({url: "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC", method: 'GET'})
+ 	.done(function(response) {
+   		console.log(response) // the json object that came back from the api
+ 	});
+
+});
 
 
 
@@ -22,8 +32,5 @@ for(i=0; i<list.length; i++){
 
 
 
-
-
-
-    
+//end of document ready    
 });
